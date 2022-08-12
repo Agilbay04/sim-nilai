22ff97c9a22fcdd87d3c650dda6ad003882c1db1
Bayu Agil Prananda <bayuagil04@gmail.com>
2022-08-13 03:58:43 +0700

    database on folder /db

---
+++ db/sim-nilai.sql
@@ -0,0 +1,368 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 12, 2022 at 10:57 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sim-nilai`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `nama_admin` varchar(225) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nama_admin`, `id_user`) VALUES
+(1, 'Nanda Pratama', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guru`
+--
+
+CREATE TABLE `guru` (
+  `id_guru` int(11) NOT NULL,
+  `nama_guru` varchar(225) NOT NULL,
+  `NIP` varchar(20) NOT NULL,
+  `id_matpel` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `guru`
+--
+
+INSERT INTO `guru` (`id_guru`, `nama_guru`, `NIP`, `id_matpel`, `id_user`) VALUES
+(1, 'Endang Putri', '1114445556', 2, 2),
+(4, 'Budi Setiawan', '12345678910', 5, 10),
+(5, 'Sulis', '10987654321', 1, 11),
+(6, 'Eko', '5556667778', 3, 12),
+(7, 'Mahmud', '6664443332', 4, 13);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `kelas` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `kelas`) VALUES
+(1, '8'),
+(2, '9'),
+(3, '10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matpel`
+--
+
+CREATE TABLE `matpel` (
+  `id_matpel` int(11) NOT NULL,
+  `matpel` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `matpel`
+--
+
+INSERT INTO `matpel` (`id_matpel`, `matpel`) VALUES
+(1, 'IPA'),
+(2, 'IPS'),
+(3, 'Bahasa Inggris'),
+(4, 'Bahasa Indonesia'),
+(5, 'Matematika');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nilai`
+--
+
+CREATE TABLE `nilai` (
+  `id_nilai` int(11) NOT NULL,
+  `id_siswa` int(11) NOT NULL,
+  `id_matpel` int(11) NOT NULL,
+  `nilai` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `nilai`
+--
+
+INSERT INTO `nilai` (`id_nilai`, `id_siswa`, `id_matpel`, `nilai`) VALUES
+(141, 17, 1, 0),
+(142, 17, 2, 70),
+(143, 17, 3, 75),
+(144, 17, 4, 0),
+(145, 17, 5, 0),
+(146, 18, 1, 0),
+(147, 18, 2, 80),
+(148, 18, 3, 78),
+(149, 18, 4, 0),
+(150, 18, 5, 0),
+(151, 19, 1, 0),
+(152, 19, 2, 90),
+(153, 19, 3, 89),
+(154, 19, 4, 0),
+(155, 19, 5, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `id_role` int(11) NOT NULL,
+  `role` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id_role`, `role`) VALUES
+(1, 'Admin'),
+(2, 'Guru'),
+(3, 'Murid');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `id_siswa` int(11) NOT NULL,
+  `nama_siswa` varchar(225) NOT NULL,
+  `NIS` varchar(10) NOT NULL,
+  `kelas_id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`id_siswa`, `nama_siswa`, `NIS`, `kelas_id`, `id_user`) VALUES
+(17, 'Alfin Mursid', '12345', 1, 32),
+(18, 'Luki Fernanda', '54321', 2, 33),
+(19, 'Choirul Samsani', '33344', 3, 34);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(225) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `role_id`) VALUES
+(1, 'admin', '$2y$10$gMkwMzyjWqfRf5VZSFlPgeuvy.omwRTRZ59NtuKsfZraTbpMrL3BG', 1),
+(2, 'endang', '$2y$10$mR69zLKXK8j8rB5sRHGr7ubWWgal5F5f9snuNZiWn1PyJW9JD9Jwi', 2),
+(10, 'budiawan', '$2y$10$IS31vL.E1Hh/ubz5hWddv.10iZRT5iU7rrQ1rVn5WzyYbLvnmz6TK', 2),
+(11, 'sulis', '$2y$10$6lFdoB36LVx9Lj0AP.82EeQyiRoV9BZzAx2a3LmgUsg0.EU5C2p8K', 2),
+(12, 'eko123', '$2y$10$GiBYDodkCP99As5n1GKcEOZuO21xZw6.aNO60I5A3k60wHApGvAEC', 2),
+(13, 'mahmud', '$2y$10$VRE7SfOCSngF44a6fqSEG.hwjhue7Itfvd8vlXHET9Md4ObZkuaQ6', 2),
+(32, 'alfin', '$2y$10$NJY8qQj2PxY03DAdYpcA2.RUl0e/uQ5DajXwrD1/SgXTwv6IzRL7K', 3),
+(33, 'lukii', '$2y$10$nOi1TqlGWlLwXrs4Ea08geAFH3unXHeJH4PfocN3uHGoGe.4TyW/W', 3),
+(34, 'choirul', '$2y$10$N2FXLNEBa.A8I0HJfCtMhe6yakTA4ZJg4YfJdfUzNCZrBo9VaDqIW', 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `guru`
+--
+ALTER TABLE `guru`
+  ADD PRIMARY KEY (`id_guru`),
+  ADD KEY `id_matpel` (`id_matpel`,`id_user`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indexes for table `matpel`
+--
+ALTER TABLE `matpel`
+  ADD PRIMARY KEY (`id_matpel`);
+
+--
+-- Indexes for table `nilai`
+--
+ALTER TABLE `nilai`
+  ADD PRIMARY KEY (`id_nilai`),
+  ADD KEY `id_user` (`id_siswa`,`id_matpel`),
+  ADD KEY `id_siswa` (`id_siswa`),
+  ADD KEY `id_matpel` (`id_matpel`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id_role`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`id_siswa`),
+  ADD KEY `kelas_id` (`kelas_id`,`id_user`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `guru`
+--
+ALTER TABLE `guru`
+  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `matpel`
+--
+ALTER TABLE `matpel`
+  MODIFY `id_matpel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `nilai`
+--
+ALTER TABLE `nilai`
+  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=156;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `admin`
+--
+ALTER TABLE `admin`
+  ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `guru`
+--
+ALTER TABLE `guru`
+  ADD CONSTRAINT `guru_ibfk_1` FOREIGN KEY (`id_matpel`) REFERENCES `matpel` (`id_matpel`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `guru_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `nilai`
+--
+ALTER TABLE `nilai`
+  ADD CONSTRAINT `nilai_ibfk_1` FOREIGN KEY (`id_siswa`) REFERENCES `siswa` (`id_siswa`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `nilai_ibfk_2` FOREIGN KEY (`id_matpel`) REFERENCES `matpel` (`id_matpel`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `siswa_ibfk_2` FOREIGN KEY (`kelas_id`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id_role`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
